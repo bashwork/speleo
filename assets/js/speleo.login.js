d956dbc01b43e5fe905d6c19ea31e2ee8058b925
Galen Collins <bashwork@gmail.com>
2012-03-15 13:39:34 -0700

    adding login page

---
+++ assets/js/speleo.login.js
@@ -0,0 +1,65 @@
+// ------------------------------------------------------------
+// main 
+// ------------------------------------------------------------
+var LoginAppView = Backbone.View.extend({
+  el: $('#login-form'),
+  usernameEl: $('#login-username'),
+  passwordEl: $('#login-password'),
+  events: {
+    'click .btn': 'validate',
+    'keypress .error input': 'toggleError'
+  },
+
+  initialize: function() {
+    _.bindAll(this);
+    this.blinds();
+  },
+
+  blinds: function() {
+    var h = $(window).height(),
+        w = $(window).width();
+        c = {
+        background: '#222',
+        height: h, width: w,
+        'z-index': 1050,
+        position:'fixed',
+        top: 0, left: 0
+
+    };
+    this.blind = $('<div/>').css(c).appendTo('html');
+    this.$el.fadeIn('slow');
+  },
+
+  toggleError: function(e) {
+    $(e.target).parents('.error').removeClass('error');
+  },
+
+  validate: function(e) {
+    e.preventDefault();
+    var user = this.usernameEl.val(),
+        pass = this.passwordEl.val();
+
+    if (!user) {
+      this.usernameEl.parents('.control-group').addClass('error');
+    } else if (!pass) {
+      this.passwordEl.parents('.control-group').addClass('error');
+    } else {
+      this.login(user, pass);
+    }
+  },
+
+  login: function(user, pass) {
+    this.blind.fadeOut();
+    this.$el.fadeOut();
+    //_.delay(function() {
+    //  window.location.replace('./search.html')
+    //}, 2000);
+  }
+});
+
+// ------------------------------------------------------------
+// page ready
+// ------------------------------------------------------------
+jQuery(function initialize($) {
+  window.app = new LoginAppView();
+});
